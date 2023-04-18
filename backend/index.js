13045301b33aafd7bffe1f81e2def1f46fff341b
dsd739 <durgadangi.2021cs@technonjr.org>
2023-04-18 20:46:30 +0530

    code with till 45 video  Changes to be committed: 	modified:   backend/index.js 	modified:   backend/models/Notes.js 	modified:   backend/models/User.js 	modified:   backend/package-lock.json 	modified:   backend/package.json 	new file:   backend/routes/auth.js 	new file:   backend/routes/notes.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,25 @@
-const connectToMongo=require('./db');
+const connectToMongo=require('./db'); //this is just for connect database
 const express = require('express')
 
-connectToMongo();
+connectToMongo(); //the function exists in db.js
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Ds!')
-})
+// app.get('/', (req, res) => {  
+//   res.send('Hello Ds!')
+// })
 
+//it is a middleware 
+//with the help of this we can send json 
+//without this we can't use req.body in routes
+//bassically req.body is a json or say data to send into db
+app.use(express.json())
+
+// available routes
+//these are end points
+app.use('/api/auth', require('./routes/auth'))
+app.use('api/notes', require('./routes/notes'))
+ 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
